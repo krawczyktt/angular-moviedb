2c8e1ca926b5bfebf7364bf8071a37e04285eee1
krawczyktt <krawczyktt@gmail.com>
2019-02-11 00:04:32 +0000

    Updated movie service to load MovieDB api

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
   private headers: HttpHeaders;
-  private accessPointUrl: string = 'https://jsonplaceholder.typicode.com/todos';
+  private accessPointUrl: string = 'https://api.themoviedb.org/3';
+  private apiKey: string = environment.apiKey;
 
   constructor(private http: HttpClient) {
     this.headers = new HttpHeaders({'Content-Type': 'application/json; charset=utf-8'});
@@ -14,9 +16,15 @@ export class MovieService {
 
   public get(payload = '') {
     const path = payload
-      ? `/${payload}`
-      : '';
+      ? `/movie/${payload}`
+      : '/movie/popular';
 
-    return this.http.get(`${this.accessPointUrl}${path}`, {headers: this.headers});
+    return this.http.get(`${this.accessPointUrl}${path}?api_key=${this.apiKey}`, {headers: this.headers});
+  }
+
+  public search(payload) {
+    const path = `/search/movie?query=${payload}`;
+
+    return this.http.get(`${this.accessPointUrl}${path}&api_key=${this.apiKey}`, {headers: this.headers});
   }
 }
