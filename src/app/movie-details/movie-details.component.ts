507f79f7f8a1c469ff846254958af76e4b9e71b2
krawczyktt <krawczyktt@gmail.com>
2019-02-11 00:04:10 +0000

    Movie component template

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.scss']
 })
-export class MovieDetailsComponent implements OnInit {
 
-  constructor() { }
+export class MovieDetailsComponent implements OnInit {
+  private movieData: object = {};
+
+  constructor(private route: ActivatedRoute, private movieService: MovieService) {}
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.load(params['id']);
+    });
   }
+
+  private load = id =>
+    this.movieService.get(id)
+      .subscribe((data: any) => {
+        console.log(data);
+        this.movieData = data;
+      });
 
 }
